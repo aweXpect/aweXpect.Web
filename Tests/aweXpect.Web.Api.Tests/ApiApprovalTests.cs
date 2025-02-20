ddcacff74a9b736c6ffd2e9e759311cd8db5227a
Valentin <valentin.breuss@baur.eu>
2025-02-20 07:37:19 +0100

    Fix NUnit1028: Only test methods should be public

--- Tests/aweXpect.Web.Api.Tests/ApiApprovalTests.cs
+++ Tests/aweXpect.Web.Api.Tests/ApiApprovalTests.cs
@@ -20,7 +20,7 @@ public sealed class ApiApprovalTests
 		await That(publicApi).IsEqualTo(expectedApi);
 	}
 
-	public static IEnumerable<string> TargetFrameworksTheoryData()
+	private static IEnumerable<string> TargetFrameworksTheoryData()
 	{
 		List<string> theoryData = new();
 		foreach (string targetFramework in Helper.GetTargetFrameworks())
