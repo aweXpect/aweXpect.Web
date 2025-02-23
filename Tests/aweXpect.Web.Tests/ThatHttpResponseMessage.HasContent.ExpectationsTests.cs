1f0ee5b39598018716a2b4697d18234bc9161097
Valentin Breuß <vbreuss@gmail.com>
2025-02-23 16:49:40 +0100

    feat: add `HasContent` with expectations (#27)
    Add an overload of `HasContent` that accepts an `Action<IThat<string?>> expectations` parameter to specify expectations on the string content.

--- Tests/aweXpect.Web.Tests/ThatHttpResponseMessage.HasContent.ExpectationsTests.cs
+++ Tests/aweXpect.Web.Tests/ThatHttpResponseMessage.HasContent.ExpectationsTests.cs
@@ -6,7 +6,7 @@ public sealed partial class ThatHttpResponseMessage
 {
 	public sealed partial class HasContent
 	{
-		public sealed class Tests
+		public sealed class ExpectationsTests
 		{
 			[Fact]
 			public async Task WhenContentDiffersFromExpected_ShouldFail()
@@ -16,13 +16,13 @@ public sealed partial class ThatHttpResponseMessage
 					.WithContent("some content");
 
 				async Task Act()
-					=> await That(subject).HasContent(expected);
+					=> await That(subject).HasContent(which => which.IsEqualTo(expected));
 
 				await That(Act).Throws<XunitException>()
 					.WithMessage("""
 					             Expected that subject
-					             has a string content equal to "other content",
-					             but it was "some content" which differs at index 0:
+					             has a string content which is equal to "other content",
+					             but the string content was "some content" which differs at index 0:
 					                ↓ (actual)
 					               "some content"
 					               "other content"
@@ -45,7 +45,7 @@ public sealed partial class ThatHttpResponseMessage
 					.WithContent(expected);
 
 				async Task Act()
-					=> await That(subject).HasContent(expected);
+					=> await That(subject).HasContent(which => which.IsEqualTo(expected));
 
 				await That(Act).DoesNotThrow();
 			}
@@ -56,13 +56,16 @@ public sealed partial class ThatHttpResponseMessage
 				HttpResponseMessage? subject = null;
 
 				async Task Act()
-					=> await That(subject).HasContent("some content");
+					=> await That(subject).HasContent(which => which.IsEmpty());
 
 				await That(Act).Throws<XunitException>()
 					.WithMessage("""
 					             Expected that subject
-					             has a string content equal to "some content",
+					             has a string content which is empty,
 					             but it was <null>
+					             
+					             HTTP-Request:
+					             <null>
 					             """);
 			}
 		}
