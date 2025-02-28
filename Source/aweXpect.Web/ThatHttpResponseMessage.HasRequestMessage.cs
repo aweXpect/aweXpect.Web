700c604ae64d0c80b2034f5398ad7e7d229faf39
Valentin Breuß <vbreuss@gmail.com>
2025-02-28 14:42:39 +0100

    feat: add `HasRequestMessage` expectations (#39)
    Allow adding expectations for the `RequestMessage` on a `HttpResponseMessage`.

---
+++ Source/aweXpect.Web/ThatHttpResponseMessage.HasRequestMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using aweXpect.Core;
+using aweXpect.Helpers;
+using aweXpect.Results;
+
+namespace aweXpect;
+
+public static partial class ThatHttpResponseMessage
+{
+	/// <summary>
+	///     Verifies that the response has a request message which satisfies the <paramref name="expectations" />.
+	/// </summary>
+	public static AndOrResult<HttpResponseMessage, IThat<HttpResponseMessage?>>
+		HasRequestMessage(this IThat<HttpResponseMessage?> source, Action<IThat<HttpRequestMessage>> expectations)
+		=> new(source.ThatIs().ExpectationBuilder
+				.UpdateContexts(c => c.Close())
+				.ForMember(MemberAccessor<HttpResponseMessage?, HttpRequestMessage?>.FromFunc(
+						response => response?.RequestMessage,
+						"has a request message which "),
+					null,
+					false)
+				.AddExpectations(expectationBuilder =>
+						expectations(new ThatSubject<HttpRequestMessage>(expectationBuilder)),
+					ExpectationGrammars.Nested),
+			source);
+}
